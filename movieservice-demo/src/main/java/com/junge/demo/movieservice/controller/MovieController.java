adbc75b2bb007291355a4e3201e04df46ebf0c42
junge8618 <513851613@qq.com>
2019-02-16 00:05:43 +0800

    eureka添加集群

--- movieservice-demo/src/main/java/com/junge/demo/movieservice/controller/MovieController.java
+++ movieservice-demo/src/main/java/com/junge/demo/movieservice/controller/MovieController.java
@@ -21,7 +21,7 @@ public class MovieController {
     @GetMapping("/users/{id}")
     public User findById(@PathVariable Long id) {
         // 这里用到了RestTemplate的占位符能力
-        User user = this.restTemplate.getForObject("http://localhost:8000/users/{id}", User.class, id);
+        User user = this.restTemplate.getForObject("http://localhost:8001/users/{id}", User.class, id);
         // ...电影微服务的业务...
         return user;
     }
